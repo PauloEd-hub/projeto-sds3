35d0935f85b6bbce31fd3e520e6ec0c6c7e8f39d
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2021-09-12 17:10:29 -0300

    Barchart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,12 @@
 export type SaleSum ={
     
-        "sellerName": string,
-        "sum": number
+        "sellerName": string;
+        "sum": number;
     
+}
+
+export type SaleSucess = {
+        sellerName: string;
+        visited: number;
+        deals: number;
 }
